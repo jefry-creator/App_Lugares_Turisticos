b30583fd618772b98728c2c5ecb106cc1fa9b0e4
Antonio Borja <antonioborja447@gmail.com>
2024-05-20 23:14:35 -0600

    - Diseño para login y registro.
    - Boton de Google.
    
    - Autenticación de Firebase y Google.

--- app/src/main/java/com/example/app_lugares_turisticos/MainActivity.java
+++ app/src/main/java/com/example/app_lugares_turisticos/MainActivity.java
@@ -10,6 +10,20 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
 import com.example.app_lugares_turisticos.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
