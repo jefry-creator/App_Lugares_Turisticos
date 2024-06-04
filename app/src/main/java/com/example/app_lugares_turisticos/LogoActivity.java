0982295de04bf48f0a0a764250604b79efccffbd
Antonio Borja <antonioborja447@gmail.com>
2024-06-03 21:47:47 -0600

    - Agregue el Splash Activity y Logo Activity tambien le di funcionalidad a ambas ;)

--- app/src/main/java/com/example/app_lugares_turisticos/LogoActivity.java
+++ app/src/main/java/com/example/app_lugares_turisticos/LogoActivity.java
@@ -2,13 +2,38 @@ package com.example.app_lugares_turisticos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LogoActivity extends AppCompatActivity {
+
+    private Button logindButton;
+    private Button signupdButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logo);
+
+        logindButton = findViewById(R.id.logind_button);
+        signupdButton = findViewById(R.id.signupd_button);
+
+        logindButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent = new Intent(LogoActivity.this, LoginActivity.class);
+                startActivity(loginIntent);
+            }
+        });
+
+        signupdButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signupIntent = new Intent(LogoActivity.this, SignUpActivity.class);
+                startActivity(signupIntent);
+            }
+        });
     }
 }
